d12a9da6de0c7db7ce02a56578454873273d2e1f
rolandlazaro16 <rolandlazaro16@gmail.com>
2026-06-27 14:07:46 +0300

    update

--- backend/server.js
+++ backend/server.js
@@ -11,10 +11,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGODB_URI)
 .then(() => console.log('MongoDB connected to eProduct_db'))
 .catch(err => console.error('MongoDB connection error:', err));
 
